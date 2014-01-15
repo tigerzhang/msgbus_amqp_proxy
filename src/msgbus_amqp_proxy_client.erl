a6b1fb0194a5568d5547fdbd8b9b903373ad3843
Zhang Hu <iamzhanghu@gmail.com>
2014-01-15 12:43:25 +0800

    send received msg to emqtt_registry. fixme!!!

--- src/msgbus_amqp_proxy_client.erl
+++ src/msgbus_amqp_proxy_client.erl
@@ -27,7 +27,9 @@
   name,
   level,
   exchange,
-  params
+  params,
+    package_sent_count,
+    package_recv_count
 }).
 
 %% ------------------------------------------------------------------
@@ -41,14 +43,14 @@
 %% ------------------------------------------------------------------
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+  terminate/2, code_change/3]).
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 start_link({Id, Params, OutgoingQueues, IncomingQueues, NodeTag}) ->
   gen_server:start_link({local, Id}, ?MODULE, {Params, OutgoingQueues, IncomingQueues, NodeTag}, []).
@@ -58,83 +60,87 @@ start_link({Id, Params, OutgoingQueues, IncomingQueues, NodeTag}) ->
 %% ------------------------------------------------------------------
 
 init({Params, OutgoingQueues, IncomingQueues, NodeTag}) ->
-  
+
   % io:format("Params: ~p~n", [Params]),
 
-  Name = config_val(name, Params, ?MODULE),  
+  Name = config_val(name, Params, ?MODULE),
   Level = config_val(level, Params, debug),
   Exchange = config_val(exchange, Params, list_to_binary(atom_to_list(?MODULE))),
-  
-  AmqpParams = #amqp_params_network {
-    username       = config_val(amqp_user, Params, <<"guest">>),
-    password       = config_val(amqp_pass, Params, <<"guest">>),
-    virtual_host   = config_val(amqp_vhost, Params, <<"/">>),
-    host           = config_val(amqp_host, Params, "localhost"),
-    port           = config_val(amqp_port, Params, 5672)
+
+  AmqpParams = #amqp_params_network{
+    username = config_val(amqp_user, Params, <<"guest">>),
+    password = config_val(amqp_pass, Params, <<"guest">>),
+    virtual_host = config_val(amqp_vhost, Params, <<"/">>),
+    host = config_val(amqp_host, Params, "localhost"),
+    port = config_val(amqp_port, Params, 5672)
   },
 
   ?INFO("Connecting to: ~p", [Name]),
-  
+
   case amqp_channel(AmqpParams) of
-    {ok, Channel} ->  
-		
-		% declare exchange
+    {ok, Channel} ->
+
+      % declare exchange
       case amqp_channel:call(Channel,
-        #'exchange.declare'{ exchange = Exchange, type = <<"topic">> }) of
+        #'exchange.declare'{exchange = Exchange, type = <<"topic">>}) of
         #'exchange.declare_ok'{} ->
           ?INFO("declare exchange succeeded: ~p", [Exchange]);
         Return ->
           ?ERROR("declare exchange failed: ~p", [Return])
       end,
 
-	  % Subscribe incoming queues
-	 [subscribe_incoming_queues(Key, Queue, Exchange, Channel, NodeTag) || {Key, Queue} <- IncomingQueues ],
-	  
-	  declare_and_bind_outgoing_queues(Channel, Exchange, OutgoingQueues),
-	  
+      % Subscribe incoming queues
+      [subscribe_incoming_queues(Key, Queue, Exchange, Channel, NodeTag) || {Key, Queue} <- IncomingQueues],
+
+      declare_and_bind_outgoing_queues(Channel, Exchange, OutgoingQueues),
+
       pg2:join(msgbus_amqp_clients, self());
     Error ->
-	  Interval = 10,
+      Interval = 10,
       ?ERROR("amqp_channel failed. will try again after ~p s", [Interval]),
       % exit the client after 10 seconds, let the supervisor recreate it
       timer:exit_after(timer:seconds(Interval), "Connect failed"),
       Error
   end,
 
-  {ok, #state{ 
-    name = Name, 
-    level = Level, 
+  {ok, #state{
+    name = Name,
+    level = Level,
     exchange = Exchange,
-    params = AmqpParams
+    params = AmqpParams,
+    package_sent_count = 0,
+    package_recv_count = 0
   }}.
 
-handle_call({forward_to_amqp, RoutingKey, Message}, _From, #state{params = AmqpParams, exchange = Exchange} = State) ->
-	case amqp_channel(AmqpParams) of
-		{ok, Channel} ->
-			amqp_publish(Exchange, RoutingKey, Message, Channel, State);
-		_ ->
-			State
-	end,
-	{reply, ok, State};
+handle_call({forward_to_amqp, RoutingKey, Message}, _From,
+    #state{params = AmqpParams, exchange = Exchange, package_sent_count = Sent} = State) ->
+  State2 = case amqp_channel(AmqpParams) of
+    {ok, Channel} ->
+      amqp_publish(Exchange, RoutingKey, Message, Channel, State),
+        State#state{package_sent_count = Sent + 1};
+    _ ->
+      State
+  end,
+  {reply, ok, State2};
 
 handle_call({declare_bind, RoutingKey, Queue}, _From, #state{params = AmqpParams, exchange = Exchange} = State) ->
-	case amqp_channel(AmqpParams) of
-		{ok, Channel} ->
-			#'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = Queue}),
-			Binding = #'queue.bind'{queue       = Queue,
-							        exchange    = Exchange,
-	                      			routing_key = RoutingKey},
-			#'queue.bind_ok'{} = amqp_channel:call(Channel, Binding);
-		_ ->
-			State
-	end,
-	{reply, ok, State};
+  case amqp_channel(AmqpParams) of
+    {ok, Channel} ->
+      #'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = Queue}),
+      Binding = #'queue.bind'{queue = Queue,
+      exchange = Exchange,
+      routing_key = RoutingKey},
+      #'queue.bind_ok'{} = amqp_channel:call(Channel, Binding);
+    _ ->
+      State
+  end,
+  {reply, ok, State};
 
 handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+  {reply, ok, State}.
 
 handle_cast(_Msg, State) ->
-    {noreply, State}.
+  {noreply, State}.
 
 handle_info({'DOWN', Ref, Type, Pid, Info}, State) ->
   ?INFO("DOWN: ~p", [{Ref, Type, Pid, Info}]),
@@ -146,30 +152,33 @@ handle_info({'DOWN', Ref, Type, Pid, Info}, State) ->
 
   {noreply, State};
 handle_info(#'basic.consume_ok'{consumer_tag = CTag}, State) ->
-    ?INFO("Consumer Tag: ~p", [CTag]),
-    {noreply, State};
+  ?INFO("Consumer Tag: ~p", [CTag]),
+  {noreply, State};
 handle_info({#'basic.deliver'{consumer_tag = CTag,
-                              delivery_tag = DeliveryTag,
-                              exchange = Exch,
-                              routing_key = RK},
-             #amqp_msg{payload = Data} = Content}, State) ->
-    ?INFO("ConsumerTag: ~p"
-         "~nDeliveryTag: ~p"
-         "~nExchange: ~p"
-         "~nRoutingKey: ~p"
-         "~nContent: ~p"
-         "~n",
-         [CTag, DeliveryTag, Exch, RK, Content]),
-    ?INFO("Data: ~p", [Data]),
-	{noreply, State};
+  delivery_tag = DeliveryTag,
+  exchange = Exch,
+  routing_key = RK},
+  #amqp_msg{payload = Data} = Content},
+    #state{package_recv_count = Recv} = State) ->
+%%   ?INFO("ConsumerTag: ~p"
+%%   "~nDeliveryTag: ~p"
+%%   "~nExchange: ~p"
+%%   "~nRoutingKey: ~p"
+%%   "~nContent: ~p"
+%%   "~n",
+%%     [CTag, DeliveryTag, Exch, RK, Content]),
+  ?INFO("Data: ~p", [Data]),
+    %% fixme
+    gen_server:cast(emqtt_registry, {package_from_mq, Data}),
+  {noreply, State#state{package_recv_count = Recv + 1}};
 handle_info(_Info, State) ->
-    {noreply, State}.
+  {noreply, State}.
 
 terminate(_Reason, _State) ->
-    ok.
+  ok.
 
 code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+  {ok, State}.
 
 test() ->
   gen_server:call(self(),
@@ -180,12 +189,12 @@ test() ->
 %% ------------------------------------------------------------------
 
 amqp_publish(Exchange, RoutingKey, Message, Channel, State) ->
-    Publish = #'basic.publish'{ exchange = Exchange, routing_key = RoutingKey },
-    Props = #'P_basic'{ content_type = <<"application/octet-stream">> },
-    Msg = #amqp_msg{ payload = Message, props = Props },
-	amqp_channel:cast(Channel, Publish, Msg),
+  Publish = #'basic.publish'{exchange = Exchange, routing_key = RoutingKey},
+  Props = #'P_basic'{content_type = <<"application/octet-stream">>},
+  Msg = #amqp_msg{payload = Message, props = Props},
+  amqp_channel:cast(Channel, Publish, Msg),
 
-	State.
+  State.
 
 config_val(C, Params, Default) ->
   case lists:keyfind(C, 1, Params) of
@@ -197,10 +206,10 @@ config_val(C, Params, Default) ->
 
 amqp_channel(AmqpParams) ->
   case maybe_new_pid({AmqpParams, connection},
-                     fun() -> amqp_connection:start(AmqpParams) end) of
+    fun() -> amqp_connection:start(AmqpParams) end) of
     {ok, Client} ->
       maybe_new_pid({AmqpParams, channel},
-                    fun() -> amqp_connection:open_channel(Client) end);
+        fun() -> amqp_connection:open_channel(Client) end);
     Error ->
       Error
   end.
@@ -224,48 +233,48 @@ maybe_new_pid(Group, StartFun) ->
   end.
 
 subscribe_incoming_queues(Key, Queue, Exchange, Channel, NodeTag) ->
-	  case binary:last(Queue) of
-		  $_ ->
-			  ConsumeQueue = <<Queue/binary, NodeTag/binary>>;
-		  _ ->
-			  ConsumeQueue = Queue
-	  end,
-	  
-	  case amqp_channel:call(Channel, #'queue.declare'{queue = ConsumeQueue}) of
-		  #'queue.declare_ok'{} ->
-	  		Tag = amqp_channel:subscribe( Channel,
-										  #'basic.consume'{queue = ConsumeQueue,
-														   no_ack = true},
-										  self()),
-			?DEBUG("Tag: ~p", [Tag]);
-		  Return2 ->
-			  ?ERROR("declare queue failed: ~p", [Return2])
-	  end,
-	  
-	  KeyEnd = binary:last(Key),
-	  case KeyEnd of
-		  $_ ->
-			  RoutingKey = << Key/binary, NodeTag/binary >>;
-		  _ ->
-			  RoutingKey = Key
-	  end,
-	  
-	  Binding = #'queue.bind'{queue       = ConsumeQueue,
-					        exchange    = Exchange,
-	              			routing_key = RoutingKey},
-	  case amqp_channel:call(Channel, Binding) of
-		  #'queue.bind_ok'{} ->
-			  ?INFO("Bind succeeded: ~p",
-					 [{ConsumeQueue, Exchange, RoutingKey}]);
-		  Return3 ->
-			  ?ERROR("Bind failed: ~p",
-					 [{ConsumeQueue, Exchange, RoutingKey, Return3}])
-	  end.
+  case binary:last(Queue) of
+    $_ ->
+      ConsumeQueue = <<Queue/binary, NodeTag/binary>>;
+    _ ->
+      ConsumeQueue = Queue
+  end,
+
+  case amqp_channel:call(Channel, #'queue.declare'{queue = ConsumeQueue}) of
+    #'queue.declare_ok'{} ->
+      Tag = amqp_channel:subscribe(Channel,
+        #'basic.consume'{queue = ConsumeQueue,
+        no_ack = true},
+        self()),
+      ?DEBUG("Tag: ~p", [Tag]);
+    Return2 ->
+      ?ERROR("declare queue failed: ~p", [Return2])
+  end,
+
+  KeyEnd = binary:last(Key),
+  case KeyEnd of
+    $_ ->
+      RoutingKey = <<Key/binary, NodeTag/binary>>;
+    _ ->
+      RoutingKey = Key
+  end,
+
+  Binding = #'queue.bind'{queue = ConsumeQueue,
+  exchange = Exchange,
+  routing_key = RoutingKey},
+  case amqp_channel:call(Channel, Binding) of
+    #'queue.bind_ok'{} ->
+      ?INFO("Bind succeeded: ~p",
+        [{ConsumeQueue, Exchange, RoutingKey}]);
+    Return3 ->
+      ?ERROR("Bind failed: ~p",
+        [{ConsumeQueue, Exchange, RoutingKey, Return3}])
+  end.
 
 declare_and_bind_outgoing_queues(Channel, Exchange, OutgoingQueues) ->
-	[	
-		{ #'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = Queue}),
-		#'queue.bind_ok'{} = amqp_channel:call(Channel, #'queue.bind'{queue       = Queue,
-						        exchange    = Exchange,
-                      			routing_key = Key}) }
-	|| {Key, Queue} <- OutgoingQueues ].
+  [
+    {#'queue.declare_ok'{} = amqp_channel:call(Channel, #'queue.declare'{queue = Queue}),
+      #'queue.bind_ok'{} = amqp_channel:call(Channel, #'queue.bind'{queue = Queue,
+      exchange = Exchange,
+      routing_key = Key})}
+    || {Key, Queue} <- OutgoingQueues].
